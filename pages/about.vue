d407a2fa032be1a0bbe02ac87cda66e09eb5c598
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2019-04-24 15:08:04 -0700

    Nuxt create basic project

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <button class="button--grey">Cancel</button>
+    <button class="button--green">Save</button>
+  </div>
+</template>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <p>Lorem ipsum</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: parseInt(this.$route.params.id),
+      posts: [
+        {
+          id: 1,
+          title: 'Post 1'
+        },
+        {
+          id: 2,
+          title: 'Post 2'
+        },
+        {
+          id: 3,
+          title: 'Post 3'
+        }
+      ]
+    }
+  },
+  computed: {
+    post() {
+      return this.posts.find(post => post.id === this.id)
+    }
+  }
+}
+</script>
